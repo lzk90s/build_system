0222316b7109b46fb5c271e467f8522d89ef0a04
kunta <lzk90s@163.com>
2013-08-16 11:17:11 +0800

    Update prebuilt.mk

--- scripts/core/prebuilt.mk
+++ scripts/core/prebuilt.mk
@@ -10,9 +10,9 @@ include $(BUILD_SYSTEM_TOP_DIR)/map_class.mk
 include $(BUILD_SYSTEM_TOP_DIR)/list_class.mk
 
 MAKE_VERSION:=$(shell make -v | grep "GNU Make" | sed "s/[^0-9\.]*//g")
-## GNU Make Minmium Version
+## the minimum version of GNU Make
 MAKE_MIN_NEED_VERSION:=3.81
 
 result:=$(shell expr $(MAKE_VERSION) \< $(MAKE_MIN_NEED_VERSION))
 
-$(call assert-equal, $(result), 0, The minimum version for GNU Make is [$(MAKE_MIN_NEED_VERSION)])
+$(call assert-equal, $(result), 0, The minimum needed version for GNU Make is [$(MAKE_MIN_NEED_VERSION)])
