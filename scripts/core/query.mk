237c5133b2543a5c10798ac348600ea95bb40b12
lzk90s <lzk90s@163.com>
2013-09-11 15:50:11 +0800

    query.mk

--- scripts/core/query.mk
+++ scripts/core/query.mk
@@ -26,40 +26,41 @@ MODULE_ID:=$(strip $(MODULE_ID))
 query_info:
 	$(call AssertNotNull, $(MODULE_ID), MODULE_ID is null)
 	$(call banner, ID:$(MODULE_ID))
-	$(hide)echo "id                     =$(call MOD_GetID, $(MODULE_ID))"
-	$(hide)echo "name                   =$(call MOD_GetName, $(MODULE_ID))"
-	$(hide)echo "class                  =$(call MOD_GetClass, $(MODULE_ID))"
+	$(hide)echo "ID                     =$(call MOD_GetID, $(MODULE_ID))"
+	$(hide)echo "NAME                   =$(call MOD_GetName, $(MODULE_ID))"
+	$(hide)echo "CLASS                  =$(call MOD_GetClass, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "path.source            =$(call MOD_GetSourcePath, $(MODULE_ID))"
-	$(hide)echo "path.relative          =$(call MOD_GetRelativePath, $(MODULE_ID))"
-	$(hide)echo "path.intermediate      =$(call MOD_GetInterPath, $(MODULE_ID))"
-	$(hide)echo "path.install           =$(call MOD_GetInstallPath, $(MODULE_ID))"
+	$(hide)echo "PATH.SOURCE            =$(call MOD_GetSourcePath, $(MODULE_ID))"
+	$(hide)echo "PATH.RELATIVE          =$(call MOD_GetRelativePath, $(MODULE_ID))"
+	$(hide)echo "PATH.INTERMEDIATE      =$(call MOD_GetInterPath, $(MODULE_ID))"
+	$(hide)echo "PATH.INSTALL           =$(call MOD_GetInstallPath, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "file.srcs              =$(call MOD_GetSrcFiles, $(MODULE_ID))"
-	$(hide)echo "file.objs              =$(call MOD_GetObjFiles, $(MODULE_ID))"
+	$(hide)echo "FILE.SRCS              =$(call MOD_GetSrcFiles, $(MODULE_ID))"
+	$(hide)echo "FILE.OBJS              =$(call MOD_GetObjFiles, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "info.owner             =$(call MOD_GetOwner, $(MODULE_ID))"
-	$(hide)echo "info.ar                =$(call MOD_GetARTool, $(MODULE_ID))"
-	$(hide)echo "info.cc                =$(call MOD_GetCCTool, $(MODULE_ID))"
-	$(hide)echo "info.cxx               =$(call MOD_GetCXXTool, $(MODULE_ID))"
-	$(hide)echo "info.ld                =$(call MOD_GetLinkTool, $(MODULE_ID))"
-	$(hide)echo "info.cflags            =$(call MOD_GetCFlags, $(MODULE_ID))"
-	$(hide)echo "info.cxxflags          =$(call MOD_GetCXXFlags, $(MODULE_ID))"
-	$(hide)echo "info.ldflags           =$(call MOD_GetLinkFlags, $(MODULE_ID))"
-	$(hide)echo "info.inc_dirs          =$(call MOD_GetIncDirs, $(MODULE_ID))"
-	$(hide)echo "info.lib_dirs          =$(call MOD_GetLibDris, $(MODULE_ID))"
-	$(hide)echo "info.static_libs       =$(call MOD_GetStaticLibs, $(MODULE_ID))"
-	$(hide)echo "info.shared_libs       =$(call MOD_GetSharedLibs, $(MODULE_ID))"
+	$(hide)echo "INFO.OWNER             =$(call MOD_GetOwner, $(MODULE_ID))"
+	$(hide)echo "INFO.AR                =$(call MOD_GetARTool, $(MODULE_ID))"
+	$(hide)echo "INFO.CC                =$(call MOD_GetCCTool, $(MODULE_ID))"
+	$(hide)echo "INFO.CXX               =$(call MOD_GetCXXTool, $(MODULE_ID))"
+	$(hide)echo "INFO.LD                =$(call MOD_GetLinkTool, $(MODULE_ID))"
+	$(hide)echo "INFO.CFLAGS            =$(call MOD_GetCFlags, $(MODULE_ID))"
+	$(hide)echo "INFO.CXXFLAGS          =$(call MOD_GetCXXFlags, $(MODULE_ID))"
+	$(hide)echo "INFO.LDFLAGS           =$(call MOD_GetLinkFlags, $(MODULE_ID))"
+	$(hide)echo "INFO.INC_DIRS          =$(call MOD_GetIncDirs, $(MODULE_ID))"
+	$(hide)echo "info.LIB_DIRS          =$(call MOD_GetLibDris, $(MODULE_ID))"
+	$(hide)echo "info.STATIC_LIBS       =$(call MOD_GetStaticLibs, $(MODULE_ID))"
+	$(hide)echo "info.SHARED_LIBS       =$(call MOD_GetSharedLibs, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "depend.deps            =$(call MOD_GetDepends, $(MODULE_ID))"
+	$(hide)echo "DEP.DEPENDS            =$(call MOD_GetDepends, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "i_e.import_headers     =$(call MOD_GetImportHeaderNames, $(MODULE_ID))"
-	$(hide)echo "i_e.export_header      =$(call MOD_GetExportHeaderName, $(MODULE_ID))"
-	$(hide)echo "i_e.export_header_dirs =$(call MOD_GetExportHeaderDirs, $(MODULE_ID))"
+	$(hide)echo "HEADER.IMPORT_NAMES    =$(call MOD_GetImportHeaderNames, $(MODULE_ID))"
+	$(hide)echo "HEADER.EXPORT_NAME     =$(call MOD_GetExportHeaderName, $(MODULE_ID))"
+	$(hide)echo "HEADER.EXPORT_DIRS     =$(call MOD_GetExportHeaderDirs, $(MODULE_ID))"
 	$(hide)echo ""
-	$(hide)echo "target.ultimate        =$(call MOD_GetUltimateTarget, $(MODULE_ID))"
-	$(hide)echo "target.intermediate    =$(call MOD_GetInterTarget, $(MODULE_ID))"
-	
+	$(hide)echo "TARGET.ULTIMATE        =$(call MOD_GetUltimateTarget, $(MODULE_ID))"
+	$(hide)echo "TARGET.INTERMEDIATE    =$(call MOD_GetInterTarget, $(MODULE_ID))"
+	$(hide)echo ""
+
 
 .PHONY: list_task list_module list_depend list_import
 list_task:
@@ -68,11 +69,13 @@ list_task:
 		echo $${m}	;	\
 	done
 	
+
 list_module:
 	$(call banner, count:$(call SET_Count, $(SET_Module)))
 	$(hide)for m in $(call SET_GetValSet, $(SET_Module)); do \
 		echo $${m}	;	\
 	done
+
 	
 list_depend:
 	$(call banner, count:$(call SET_Count, $(SET_Depend)))
@@ -80,14 +83,18 @@ list_depend:
 		echo $${m};	\
 	done
 
+	
 list_export:
 	$(call banner, count:$(call SET_Count, $(SET_HeaderExport)))
 	$(hide)for m in $(call SET_GetValSet, $(SET_HeaderExport)); do \
 		echo $${m};	\
 	done
+
 	
 list_import:
 	$(call banner, count:$(call SET_Count, $(SET_HeaderImport)))
 	$(hide)for m in $(call SET_GetValSet, $(SET_HeaderImport)); do \
 		echo $${m};	\
 	done
+
+	
