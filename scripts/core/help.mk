1b6feb16f34e73e7ce549480f7639c5ef6f26257
kunta <lzk90s@163.com>
2016-03-08 10:37:29 +0800

    Update help.mk
    更新帮助信息

--- scripts/core/help.mk
+++ scripts/core/help.mk
@@ -6,11 +6,17 @@
 
 .PHONY: help
 help:
-	@echo "build system : $(call GetSystemVersion)"
-	@echo
-	@echo "Common make targets:"
-	@echo "----------------------------------------------------------------------------------"
-	@echo "main                                    Default target"
-	@echo "clean_module                            module clean"
-	@echo "distclean                               rm $(PROJECT_OUTPUT_TOP_DIR)" 
-	@echo "help                                    You're reading it right now"
+	$(call PrintInfoMsg,"+------------------------------------------------------------------+")
+	$(call PrintErrorMsg," build system : $(call GetSystemVersion)")
+	$(call PrintInfoMsg,"+------------------------------------------------------------------+")
+	$(call PrintInfoMsg," main  : Default target")
+	$(call PrintInfoMsg," clean_module : module clean")
+	$(call PrintInfoMsg," distclean : rm $(PROJECT_OUTPUT_TOP_DIR)")
+	$(call PrintInfoMsg," help : help information")
+	$(call PrintInfoMsg," query_info : query module info")
+	$(call PrintInfoMsg," list_task : list all tasks")
+	$(call PrintInfoMsg," list_module : list all modules")
+	$(call PrintInfoMsg," list_depend : list dependence between modules")
+	$(call PrintInfoMsg," list_export : list all header to be exported")
+	$(call PrintInfoMsg," list_import : list all header to be imported")
+	$(call PrintInfoMsg,"+------------------------------------------------------------------+")
